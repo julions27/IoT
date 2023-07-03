8dfe806210b852ebe60b57238faf78768cec5b3c
julions27 <109082651+julions27@users.noreply.github.com>
2023-07-03 20:33:08 -0300

    Create ESP32.c

---
+++ ESP32.c
@@ -0,0 +1,78 @@
+#include <WiFi.h>
+#include <ESPAsyncWebSrv.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 5    // Pino SS do módulo RFID
+#define RST_PIN 22  // Pino RST do módulo RFID
+
+MFRC522 rfid(SS_PIN, RST_PIN);   // Objeto RFID
+
+const char* ssid = "Rede";
+const char* password = "vaisaber";
+const char* serverIP = "192.168.235.1";
+
+AsyncWebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+
+  // Inicializa a conexão WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Inicializa o módulo RFID
+  SPI.begin();
+  rfid.PCD_Init();
+
+  // Rotas do servidor web
+  server.on("/cadastrar", HTTP_POST, [](AsyncWebServerRequest *request){
+    if (request->hasParam("produto", true) && request->hasParam("quantidade", true)) {
+      String produto = request->getParam("produto", true)->value();
+      String quantidade = request->getParam("quantidade", true)->value();
+
+      // Realize as operações necessárias para associar o cartão RFID ao produto e quantidade
+      // Salve as informações em um arquivo chamado "cartao.txt"
+
+      request->send(200, "text/plain", "success");
+    } else {
+      request->send(400, "text/plain", "Parâmetros inválidos");
+    }
+  });
+
+  server.on("/listar-produtos", HTTP_GET, [](AsyncWebServerRequest *request){
+    // Leia o arquivo "cartao.txt" e envie os dados como resposta
+    // Certifique-se de definir o cabeçalho "Content-Type" como "text/plain"
+
+    request->send(200, "text/plain", "Dados dos produtos");
+  });
+
+  server.on("/remover-produto", HTTP_DELETE, [](AsyncWebServerRequest *request){
+    // Realize as operações necessárias para remover o produto associado ao cartão RFID lido
+    // Verifique se o cartão está cadastrado no arquivo "cartao.txt" e remova-o
+
+    request->send(200, "text/plain", "success");
+  });
+
+  server.begin();
+}
+
+void loop() {
+  // Verifique a leitura de um cartão RFID
+  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    String cartao = "";
+    for (byte i = 0; i < rfid.uid.size; i++) {
+      cartao += String(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+      cartao += String(rfid.uid.uidByte[i], HEX);
+    }
+
+    // Realize as operações necessárias com o cartão lido (por exemplo, remover produto)
+
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();
+  }
+}
